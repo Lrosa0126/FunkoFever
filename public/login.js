29b2141fa1035c4b3484f3b51c49357b0ff07642
homelesscat <gilbertperezdt@gmail.com>
2023-07-19 11:58:00 -0700

    login.hbs updated, login.js created in the js public folder

---
+++ public/login.js
@@ -0,0 +1,22 @@
+const login = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/'); // Redirect user to home page
+    } else {
+      alert('Log in failed'); // Alert user that log in failed
+    }
+  }
+};
+
+document.querySelector('#login-form').addEventListener('submit', login);
