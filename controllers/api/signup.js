471427341e053660781de3facdc46053ef15903d
homelesscat <gilbertperezdt@gmail.com>
2023-07-26 23:28:48 -0700

    removed some unused code from server.js

--- controllers/api/signup.js
+++ controllers/api/signup.js
@@ -74,33 +74,3 @@ router.post('/logout', (req, res) => {
 });
 
 module.exports = router;
-
-// do not use code
-
-/*const signup = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector('#username-input').value.trim();
-  const password = document.querySelector('#password-input').value.trim();
-
-  if (username && password) {
-    const response = await fetch('/api/users/signup', {
-      method: 'POST',
-      body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-    console.log(response);
-
-    if (response.ok) {
-      document.location.replace('/'); // Redirect user to home page
-    } else {
-      alert('Failed to sign up.'); // Alert user that sign-up failed
-    }
-  }
-};
-
-
-
-document.querySelector('#signup-form').addEventListener('submit', signup);
-
-//copied from public\js\login.js */
